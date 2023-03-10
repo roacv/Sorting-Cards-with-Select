036e5b297160d19f410bf9aae33eb24a35b5ac46
roacv <roacv@hotmail.com>
2023-03-10 06:04:41 +0000

    V Final

--- src/app.js
+++ src/app.js
@@ -13,40 +13,25 @@ window.onload = function() {
   let rannum = num[Math.floor(Math.random() * num.length)];
   let ranpin = pin[Math.floor(Math.random() * pin.length)];
   let cards = [];
-  document.getElementById("imgtop").src = "../src/assets/img/" + ranpin;
-  document.getElementById("imgbot").src = "../src/assets/img/" + ranpin;
-  numero.innerHTML = rannum;
-
+  // generamos array con cartas random
   document.getElementById("sorteo").addEventListener("click", function() {
     let num = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
     let pin = ["corazon.png", "diamante.png", "pica.png", "trebol.png"];
-    cartas.innerHTML = "";
     for (let i = 0; i < cantCards.value; i++) {
       let rannum = num[Math.floor(Math.random() * num.length)];
       let ranpin = pin[Math.floor(Math.random() * pin.length)];
       cards[i] = { pinta: ranpin, numero: rannum };
-      cartas.innerHTML +=
-        "<div class='carta'><img id='imgtop" +
-        i +
-        "' class='pintatop' alt=''/><p id='numero" +
-        i +
-        "'" +
-        "class='numero'></p><img id='imgbot" +
-        i +
-        "' class='pintabot' alt=''/></div>";
-      document.getElementById("imgtop" + i).src = "../src/assets/img/" + ranpin;
-      document.getElementById("numero" + i).innerHTML = cards[i].numero;
-      document.getElementById("imgbot" + i).src = "../src/assets/img/" + ranpin;
     }
+    // renderizamos el array del sorteo
+    renderCards(cards, "cartas");
   });
+  // rutina de clasificación y creación de log
   document.getElementById("sort").addEventListener("click", function() {
     let cardsord = [];
     let cardsn = [].concat(cards);
     let paso = "";
     let ind = 0;
     order.innerHTML = "";
-    console.log("vemos las cartas");
-    console.log(cards);
     for (let i = 1; i <= cantCards.value; i++) {
       for (let j = 0; j < cantCards.value - 1; j++) {
         if (numcar(cardsn[j].numero) > numcar(cardsn[j + 1].numero)) {
@@ -59,25 +44,11 @@ window.onload = function() {
           cardsn[j] = paso;
         }
       }
-      // order.innerHTML +=
-      //   "<div class='carta'><img id='imgtopo" +
-      //   i +
-      //   "' class='pintatop' alt=''/><p id='numeroo" +
-      //   i +
-      //   "'" +
-      //   "class='numero'></p><img id='imgboto" +
-      //   i +
-      //   "' class='pintabot' alt=''/></div>";
-      // document.getElementById("imgtopo" + i).src =
-      //   "../src/assets/img/" + cards[i].pinta;
-      // document.getElementById("numeroo" + i).innerHTML = cards[i].numero;
-      // document.getElementById("imgboto" + i).src =
-      //   "../src/assets/img/" + cards[i].pinta;
     }
-    console.log("Ordenado");
-    console.log(cardsn);
-    console.log("cambios");
-    console.log(cardsord);
+    // renderizamos cartas ordenadas
+    renderCards(cardsn, "order");
+    // renderizamos el log de cambios
+    renderCards(cardsord, "log");
     function numcar(num) {
       if (num == "A") {
         return 1;
@@ -94,4 +65,27 @@ window.onload = function() {
       return num;
     }
   });
+  // función de renderizado, recibe array y ID
+  function renderCards(array, id) {
+    document.getElementById(id).innerHTML = "";
+    for (let i = 0; i < array.length; i++) {
+      document.getElementById(id).innerHTML +=
+        "<div class='carta'><img id='imgtop" +
+        id +
+        i +
+        "' class='pintatop' alt=''/><p id='numero" +
+        id +
+        i +
+        "'" +
+        "class='numero'></p><img id='imgbot" +
+        id +
+        i +
+        "' class='pintabot' alt=''/></div>";
+      document.getElementById("imgtop" + id + i).src =
+        "../src/assets/img/" + array[i].pinta;
+      document.getElementById("numero" + id + i).innerHTML = array[i].numero;
+      document.getElementById("imgbot" + id + i).src =
+        "../src/assets/img/" + array[i].pinta;
+    }
+  }
 };
